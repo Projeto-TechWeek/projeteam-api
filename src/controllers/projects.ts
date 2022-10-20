0debbc279ad5830cb215dfb22503ebeab4ca51b3
nanaboo3 <93357665+nanaboo3@users.noreply.github.com>
2022-10-20 14:27:00 -0300

    ✨ feat: estrutura básica de controllers
    - Criados os controllers users.ts e projects.ts
    - Adicionado os caminhos ao server.ts

---
+++ src/controllers/projects.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+const projectsRouter = Router();
+
+projectsRouter.get("/", (_request, _response) => {
+})
+
+projectsRouter.post("/", (_request, _response) => {
+})
+
+projectsRouter.get("/:id", (_request, _response) => {
+})
+
+projectsRouter.patch("/:id", (_request, _response) => {
+})
+
+export { projectsRouter };
---
+++ src/controllers/users.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+const usersRouter = Router();
+
+usersRouter.get("/", (_request, _response) => {
+})
+
+usersRouter.post("/", (_request, _response) => {
+})
+
+usersRouter.get("/:id", (_request, _response) => {
+})
+
+usersRouter.patch("/:id", (_request, _response) => {
+})
+
+export { usersRouter };
