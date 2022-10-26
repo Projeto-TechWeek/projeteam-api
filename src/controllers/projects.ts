64fbcb63b94f7ce334932c90455f8cc6e474db45
Gabriel Souza <ga.ferreira.desouza@gmail.com>
2022-10-26 16:45:09 -0300

    :sparkles: feat: Criação de projeto

--- src/controllers/projects.ts
+++ src/controllers/projects.ts
@@ -1,24 +1,34 @@
 import { Router } from 'express';
 import { PrismaClient } from '@prisma/client';
+
+import { BadRequestError } from '../utils/errors/BadRequestError';
+import { ApiError } from '../utils/errors/ApiError';
 const prisma = new PrismaClient();
 
 const projectsRouter = Router();
 
-projectsRouter.get('/', async (_request, _response) => {
-  try {
-    const projects = await prisma.project.findMany();
+projectsRouter.get('/', async (_request, response) => {
+  const projects = await prisma.project.findMany();
 
-    projects.sort((a, b) => {
-      return b.createdAt.getTime() - a.createdAt.getTime();
-    });
+  projects.sort((a, b) => {
+    return b.createdAt.getTime() - a.createdAt.getTime();
+  });
 
-    _response.send(projects);
-  } catch (err) {
-    _response.status(500).send({ message: 'Ocorreu um erro ao listar projetos' });
-  }
+  response.status(200).send(projects);
 });
 
-projectsRouter.post('/', (_request, _response) => {
+projectsRouter.post('/', async (request, response) => {
+  const { title, description, methodology, area } = request.body;
+
+  if (!title || !description || !methodology || !area) {
+    throw new BadRequestError('É preciso preencher todos os campos');
+  }
+  try {
+    const project = await prisma.project.create({ data: { title, description, methodology, area } });
+    response.status(201).send(project);
+  } catch (err) {
+    throw new ApiError('Ocorreu um erro ao criar o projeto');
+  }
 });
 
 projectsRouter.get('/:id', (_request, _response) => {});
